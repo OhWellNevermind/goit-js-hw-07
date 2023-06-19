0fa6be07a259f87604f5d392c3e7182c7e6e47d8
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-19 19:55:39 +0300

    tasl-1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryPhotos = [...galleryItems].reduce((acc, currentValue) => {
+  return (
+    acc +
+    `<li class="gallery__item"><a class="gallery__link" href="${currentValue.original}">
+      <img class="gallery__image" src="${currentValue.preview}" alt="${currentValue.description}" data-source="${currentValue.original}"/>
+    </a></li>`
+  );
+}, "");
+
+let bigImageLink = "";
+gallery.insertAdjacentHTML("beforeend", galleryPhotos);
+gallery.addEventListener("click", imageClickHandler);
+
+function imageClickHandler(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  //   event.target.src = event.target.dataset["source"];
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`
+  );
+
+  instance.show();
+
+  gallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && instance.visible()) {
+      instance.close();
+    }
+  });
+}
+
+console.log(galleryPhotos);
