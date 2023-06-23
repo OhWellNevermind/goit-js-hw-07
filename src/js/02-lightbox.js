26644ea06a08c5e15507684258b53ab3b7198445
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-23 12:58:52 +0300

    task-2 completed

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryPhotos = [...galleryItems].reduce((acc, currentValue) => {
+  return (
+    acc +
+    `<li class="gallery__item"><a class="gallery__link" href="${currentValue.original}">
+      <img class="gallery__image" src="${currentValue.preview}" alt="${currentValue.description}" title="${currentValue.description}"/>
+    </a></li>`
+  );
+}, "");
+
+gallery.insertAdjacentHTML("beforeend", galleryPhotos);
+
+let gallerySimple = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  animationSpeed: 10,
+  fadeSpeed: 100,
+});
+console.log(gallerySimple);
